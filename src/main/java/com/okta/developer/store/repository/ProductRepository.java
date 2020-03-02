b37675b77b4d1efb3373aa36c61e6520cf70ba3c
fan <fans_2046@126.com>
2020-03-02 15:35:46 +0800

    first commit

---
+++ src/main/java/com/okta/developer/store/repository/ProductRepository.java
@@ -0,0 +1,15 @@
+package com.okta.developer.store.repository;
+
+import com.okta.developer.store.domain.Product;
+import org.springframework.data.jpa.repository.*;
+import org.springframework.stereotype.Repository;
+
+
+/**
+ * Spring Data  repository for the Product entity.
+ */
+@SuppressWarnings("unused")
+@Repository
+public interface ProductRepository extends JpaRepository<Product, Long> {
+
+}
